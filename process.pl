f84ff23ac44be7e6693dc6cbb16fd7d53bc99e1d
Hevanafa <dhevansafa4@gmail.com>
2025-12-02 10:54:17 +0700

    added process script output

--- process.pl
+++ process.pl
@@ -8,6 +8,12 @@ my $outFile = "words_5letters.txt";
 open (my $fh, "<", $srcFile) or die "Couldn't open $srcFile!";
 
 my @lines = map { chomp; $_ } <$fh>;
-say $lines[1];
+# say $lines[1];
+close $fh;
+
+open (my $fh, ">", $outFile) or die "Couldn't open $outFile for output!";
+
+@lines = grep { 5 == length } @lines;
+say $fh $_ foreach @lines;
 
 close $fh;

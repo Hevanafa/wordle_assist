005601fea6f05acab5a72a4afac1a037e791ca82
Hevanafa <dhevansafa4@gmail.com>
2025-12-02 10:56:40 +0700

    added process script output text

--- process.pl
+++ process.pl
@@ -5,15 +5,27 @@ use 5.32.1;
 my $srcFile = "TWL06.txt";
 my $outFile = "words_5letters.txt";
 
-open (my $fh, "<", $srcFile) or die "Couldn't open $srcFile!";
+my $fh;
 
+open ($fh, "<", $srcFile) or die "Couldn't open $srcFile!";
+
+say "Loading $srcFile...";
 my @lines = map { chomp; $_ } <$fh>;
 # say $lines[1];
 close $fh;
 
-open (my $fh, ">", $outFile) or die "Couldn't open $outFile for output!";
+open ($fh, ">", $outFile) or die "Couldn't open $outFile for output!";
 
+say "Writing to $outFile...";
 @lines = grep { 5 == length } @lines;
-say $fh $_ foreach @lines;
+
+my $count = 0;
+# say $fh $_ foreach @lines;
+foreach my $line (@lines) {
+  say $fh $line;
+  $count++
+}
+
+say "Written ".$count." lines to $outFile";
 
 close $fh;

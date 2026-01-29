1a1b2c842e792d1a20b9be1dc43f1de251595d7f
Hevanafa <dhevansafa4@gmail.com>
2026-01-29 11:12:16 +0700

    finalised word frequency list filter

--- build_freqlist.pl
+++ build_freqlist.pl
@@ -1,9 +1,14 @@
+# Build Frequency List Script
+# By Hevanafa, 29-01-2026
+
+# This script produces a TXT file based on the frequency list from norvig.com
+
 use strict;
 use warnings;
 use 5.32.1;
 
-my $srcFile = "freqlist.txt";
-my $outFile = "freqlist_filtered.txt";
+my $srcFile = "count_1w.txt";
+my $outFile = "freqlist_5letters.txt";
 
 my $fh;
 
@@ -13,4 +18,20 @@ say "Loading $srcFile...";
 my @lines = map { chomp; $_ } <$fh>;
 close $fh;
 
-say "Found " . (scalar @lines) . " lines";
+say "Loaded " . (scalar @lines) . " lines";
+
+@lines = grep {
+  my @pair = $_ =~ /[^\t]+/g;
+  length($pair[0]) == 5
+} @lines;
+
+my $count = 0;
+open ($fh, ">", $outFile) or die "Couldn't open $outFile for output!";
+
+foreach my $line (@lines) {
+  say $fh $line;
+  $count++
+}
+
+close $fh;
+say "Written $count lines to $outFile"
